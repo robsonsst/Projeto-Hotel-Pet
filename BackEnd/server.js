b1c9756c362ed5492454fbdf0be5ddba80068136
Harilson Pires <87832061+HarilsonOliver@users.noreply.github.com>
2021-10-22 12:37:28 -0300

    comecei a criar as rotas

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,34 +1,36 @@
 const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
 
-const sqlite3 = require('sqlite3').verbose();
-
-let db = new sqlite3.Database('db.sqlite', (err)=>{
-    if(err){
-        return console.error(err.message);
-    }
-    console.log('conectado com sucesso')
-});
-
+app.use(cors());
 const app = express();
 app.use(express.json());
 
 // aqui começa a criar as solicitações post get, pot, delet
 //criação das rotas da tela usuario
 
-
-
-app.get('/users', (req, res)=>{ //metodo get da tela usuario clicando no botão pesquisar
-
+app.get('/users', (req, res) =>{//metodo get da tela usuario clicando no botão pesquisar
     const parametro = req.query;
-
+    console.log(parametro)
     res.json(parametro)
-} ) //retorna recebendo todos os parametros
+})//retorna recebendo todos os parametros
 
-app.get('/users/:id', (req, res)=>{ //metodo get da tela usuario clicando no botão pesquisar
-
-    const parametro = req.query;
-
+app.post('/users/:id', (req, res) =>{
+    const parametro = req.params;
+    console.log(parametro)
     res.json(parametro)
-} ) //retorna recebendo Id como query
+})
+
+app.post('/users/', (req, res) =>{
+    const parametro = req.body;
+    console.log(parametro)
+    res.json(parametro)
+})
+
+app.put('/users', (req, res) =>{
+    res.json(parametro)
+})
+
+app.use(routes);
 
 app.listen(3001);
