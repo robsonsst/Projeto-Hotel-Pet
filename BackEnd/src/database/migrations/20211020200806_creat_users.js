b5a2cceadd8abccc4d67d39f35ccf6b8e0147df6
Perseu <josimar.17perseu@gmail.com>
2021-10-23 22:40:17 -0300

    criação do banco de dados, apenas a tablea usuario foi criada, as outras deram erro na criaçao da chave estrangeira

--- BackEnd/src/database/migrations/20211020200806_creat_users.js
+++ BackEnd/src/database/migrations/20211020200806_creat_users.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('usuario', function (table) {
+        table.increments('id');
+        table.string('nome').notNullable();
+        table.string('sobrenome').notNullable();
+        table.string('email').notNullable();
+        table.string('telefone');
+        table.enu('status', ['ativo', 'inativo']);
+        table.date('criado_em');
+        table.string('funcao');
+        table.string('usuario');
+        table.string('senha');
+      })
+
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTable('usuario');
   
 };
