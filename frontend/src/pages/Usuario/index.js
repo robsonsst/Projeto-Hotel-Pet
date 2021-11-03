0a169df01b733a25515891cdbff30c58f003747b
Perseu <josimar.17perseu@gmail.com>
2021-11-03 16:42:01 -0300

    correção no form de usuario, cadastrando usuario

--- frontend/src/pages/Usuario/index.js
+++ frontend/src/pages/Usuario/index.js
@@ -62,7 +62,6 @@ export default function Usuario() {
                                 <label>Criado em:
                                     <input class="componentes" type="date" ></input>
                                 </label>
-                                <input class="componentes" type="date" ></input>
 
                             </section>
 
